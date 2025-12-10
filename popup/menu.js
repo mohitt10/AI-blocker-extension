039124e95b7dd27e78fbafaf8fb3baf72d2d4c56
Mohit Jain <Mohitj8732@gmail.com>
2025-12-11 00:19:57 +0530

    Done addding basic popup layout

--- popup/menu.js
+++ popup/menu.js
@@ -1,1 +1,14 @@
 // Contains the code for popup
+const checkbox = document.getElementById("check");
+const onText = document.getElementById("ontext");
+const offText = document.getElementById("offtext");
+
+checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+        onText.classList.add("hidden");
+        offText.classList.remove("hidden");
+    } else {
+        onText.classList.remove("hidden");
+        offText.classList.add("hidden");
+    }
+});
